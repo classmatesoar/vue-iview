b895c4bc9e3cdc66d0015e503d6237f3c58535d1
苏鹏飞 <supengfei@supengfeideMacBook-Pro.local>
2018-04-02 23:54:29 +0800

    detail

--- src/components/page/login.vue
+++ src/components/page/login.vue
@@ -1,15 +1,27 @@
 <template>
     <Layout class="layout full-box">
         <Content class="content">
-            <Form class="tac">
-                <FormItem></FormItem>
-                <FormItem></FormItem>
-                <FormItem>
-                    <Button type="success" @click="loginIn">登陆</Button>
-                    <Button type="info">注册</Button>
-                    <Button type="text" class="text-color-p">忘记密码</Button>
-                </FormItem>
-            </Form>
+            <Row class="full-height" type="flex" justify="center" align="middle">
+                <Col :lg="4" :md="6" :sm="16" :xs="16">
+                    <Form class="tac full-height" ref="formInline" :model="formInline" :rules="ruleInline">
+                        <FormItem prop="user">
+                            <Input type="text" v-model="formInline.user" placeholder="请输入用户名">
+                                <Icon type="ios-person-outline" slot="prepend"></Icon>
+                            </Input>
+                        </FormItem>
+                        <FormItem prop="password">
+                            <Input type="password" v-model="formInline.password" placeholder="请输入密码">
+                                <Icon type="ios-locked-outline" slot="prepend"></Icon>
+                            </Input>
+                        </FormItem>
+                        <FormItem>
+                            <Button type="primary" @click="loginIn('formInline')">登陆</Button>
+                            <Button type="info">注册</Button>
+                            <Button type="text" class="text-color-p">忘记密码</Button>
+                        </FormItem>
+                    </Form>
+                </Col>
+            </Row>
         </Content>
         <Footer>
             <app-footer></app-footer>
@@ -18,14 +30,53 @@
 </template>
 <script>
 import appFooter from "../main/footer";
+import {iInput,iCol} from 'iview';
 export default {
   components: {
-    appFooter
+    appFooter,
+    iInput,
+    iCol
+  },
+  data() {
+    return {
+      formInline: {
+        user: "",
+        password: ""
+      },
+      ruleInline: {
+        user: [
+          {
+            required: true,
+            message: "用户名不能为空",
+            trigger: "blur"
+          }
+        ],
+        password: [
+          {
+            required: true,
+            message: "密码不能为空",
+            trigger: "blur"
+          },
+          {
+            type: "string",
+            min: 6,
+            message: "密码长度不小于6",
+            trigger: "blur"
+          }
+        ]
+      }
+    };
   },
   methods: {
-    loginIn() {
-      this.$Message.success("欢迎登陆");
-      this.$router.push("index");
+    loginIn(name) {
+      this.$refs[name].validate(valid => {
+        if (valid) {
+          this.$Message.success("登陆成功，欢迎");
+          this.$router.push('index')
+        } else {
+          this.$Message.error("Fail!");
+        }
+      });
     }
   }
 };
